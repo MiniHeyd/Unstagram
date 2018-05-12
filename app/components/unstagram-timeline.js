d2a11ea0f7d14a99a04ceb4955ec7e0e5a409e42
Steven Heydweiller <steve@highdwellercreative.com>
2018-05-11 23:26:28 -0500

    add the insta scroll functionality

--- app/components/unstagram-timeline.js
+++ app/components/unstagram-timeline.js
@@ -1,7 +1,21 @@
 import Component from '@ember/component';
+import _ from 'lodash';
 
 export default Component.extend({
   likes: 0,
   photographer: null,
-  photoUrl: null
+  photoUrl: null,
+
+  didInsertElement() {
+    let scrollTimeout,
+        offset = window.innerHeight;
+    let scroll = _.debounce(() => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+          windowHeight = window.innerHeight,
+          documentHeight = document.documentElement.scrollHeight,
+          trigger = (scrollTop + windowHeight) >= (documentHeight - offset);
+      if(trigger) this.sendAction('getPhotos');
+    });
+    window.onscroll = scroll;
+  }
 });
